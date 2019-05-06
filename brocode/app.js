6464cf6eeba3420066bbcfcb4ca89662654b90f0
Manu Arora <mannuarora700@gmail.com>
2019-05-06 15:43:07 +0530

    Working contact form // Validation done // Transaction emails now working

--- brocode/app.js
+++ brocode/app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var expressValidator = require('express-validator');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -16,6 +17,9 @@ app.set('view engine', 'hbs');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+app.use(expressValidator());
+
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
--- brocode/routes/index.js
+++ brocode/routes/index.js
@@ -1,9 +1,52 @@
 var express = require('express');
 var router = express.Router();
 
+var nodemailer = require('nodemailer');
+var config = require('../config');
+var transporter = nodemailer.createTransport(config.mailer);
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Brocode' });
+  res.render('index', { title: 'Brocode - A Platform For Pair Programming' });
 });
 
+router.get('/about', function(req, res, next) {
+	res.render('about', {title: 'Brocode - A Platform For Pair Programming'});
+});
+
+router.route('/contact')
+	.get(function(req, res, next) {
+		res.render('contact', {title: 'Brocode - A Platform For Pair Programming'});
+	})
+	.post(function(req, res, next) {
+		req.checkBody('name', 'Empty name').notEmpty();
+		req.checkBody('email', 'Invalid email').isEmail();
+		req.checkBody('message', 'Empty message').notEmpty();
+		var errors = req.validationErrors();
+
+		if(errors) {
+			res.render('contact', {
+				title: 'Brocode - A Platform For Pair Programming',
+				name: req.body.name,
+				email: req.body.email,
+				message: req.body.message,
+				errorMessages: errors
+			});
+		} else {
+			var mailOptions = {
+				from: 'Brocode <noreply@brocode.com>',
+				to: 'manu.workmailid@gmail.com',
+				subject: 'You got a new message from visitor ✒️',
+				text: req.body.message
+			};
+			transporter.sendMail(mailOptions, function(error, info) {
+				if(error) {
+					return console.log(error);
+				}
+				
+				res.render('thank', {title: 'Brocode - A Platform For Pair Programming'});
+
+			});
+		}
+	});
 module.exports = router;
